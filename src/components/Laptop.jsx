8f85aaa67353bccff0054bbbafab2a3381091cfb
Mert <mertsak2@gmail.com>
2022-09-28 13:50:46 +0300

    laptop page settings

--- src/components/Laptop.jsx
+++ src/components/Laptop.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import CurrencyFormat from "react-currency-format";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 import Rating from "@mui/material/Rating";
 const Laptop = () => {
@@ -17,37 +18,41 @@ const Laptop = () => {
     <>
       <div className="laptops__container">
         {Laptops.map((x) => (
-          <div className="laptop__container">
-            
-            <img
-              className="laptop__image"
-              src={require(`../assets/LaptopImage/${x.image}`)}
-              alt=""
-            />
+          <>
+            <div className="laptop__container">
+              <img
+                className="laptop__image"
+                src={require(`../assets/LaptopImage/${x.image}`)}
+                alt=""
+              />
 
-            <div className="laptop__info">
-              <p className="laptop__desc">
-                <strong>{x.brand}</strong> {x.name} {truncate(x.description)}
-              </p>
+              <div className="laptop__info">
+                <p className="laptop__desc">
+                  <strong>{x.brand}</strong> {x.name} {truncate(x.description)}
+                </p>
 
-              <div className="rating">
-                <Rating name="read-only" value={x.rating} readOnly />
-                <span className="reviews">
-                  {x.numReviews} <RemoveRedEyeIcon></RemoveRedEyeIcon>
-                </span>
+                <div className="rating">
+                  <Rating name="read-only" value={x.rating} readOnly />
+                  <span className="reviews">
+                    {x.numReviews} <RemoveRedEyeIcon></RemoveRedEyeIcon>
+                  </span>
+                </div>
+                <div className="price">
+                  <p>Basket Price</p>
+                  <CurrencyFormat
+                    value={x.price}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"$"}
+                  />
+                </div>
               </div>
-              <div className="price">
-                <p>Basket Price</p>
-                <CurrencyFormat
-                  value={x.price}
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  prefix={"$"}
-                />
+
+              <div className="heart">
+                <FavoriteBorderOutlinedIcon className="heart__icon"></FavoriteBorderOutlinedIcon>
               </div>
             </div>
-
-          </div>
+          </>
         ))}
       </div>
     </>
