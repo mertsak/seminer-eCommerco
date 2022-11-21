91e2c10b892dc563cc96df803db9544574e65314
Mert <mertsak2@gmail.com>
2022-11-21 23:55:43 +0300

    filter sett

--- src/components/Laptop.jsx
+++ src/components/Laptop.jsx
@@ -11,65 +11,58 @@ import Rating from "@mui/material/Rating";
 import { addFavorites } from "../redux/commerceSlice.js";
 
 const Laptop = () => {
-  const dispatch = useDispatch();
-  const Laptops = useSelector((state) => state.commerce.LaptopData);
+	const dispatch = useDispatch();
+	const { filteredLaptopData, LaptopData } = useSelector((state) => state.commerce);
 
-  const truncate = (str) => {
-    return str.length > 10 ? `${str.substring(0, 40)}...` : str;
-  };
+	const truncate = (str) => {
+		return str.length > 10 ? `${str.substring(0, 40)}...` : str;
+	};
 
-  return (
-    <>
-      <div className="products__inner__container">
-        {Laptops.map((x) => (
-          <div className="product__top__container">
-            <Link className="product__inner__container" to={`${x.id}`}>
-              <img
-                className="product__image"
-                src={require(`../assets/LaptopImage/${x.image}`)}
-                alt=""
-              />
+	const laptopData = filteredLaptopData.length > 0 ? filteredLaptopData : LaptopData;
 
-              <div className="product__info">
-                <p className="product__desc">
-                  <strong>{x.name} </strong> {truncate(x.description)}
-                </p>
+	return (
+		<>
+			<div className='products__inner__container'>
+				{laptopData.map((x) => (
+					<div className='product__top__container'>
+						<Link className='product__inner__container' to={`${x.id}`}>
+							<img className='product__image' src={require(`../assets/LaptopImage/${x.image}`)} alt='' />
 
-                <div className="rating">
-                  <Rating name="read-only" value={x.rating} readOnly />
-                  <span className="reviews">
-                    {x.numReviews} <RemoveRedEyeIcon></RemoveRedEyeIcon>
-                  </span>
-                </div>
-                <div className="price">
-                  <p>Basket Price</p>
-                  <CurrencyFormat
-                    value={x.price}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    prefix={"$"}
-                  />
-                </div>
-              </div>
-            </Link>
-            <div className="heart">
-              <Checkbox
-                onClick={() => dispatch(addFavorites(x.productUnicId))}
-                sx={{
-                  "&.Mui-checked": {
-                    color: orange[600],
-                  },
-                }}
-                checked={x.chechFavorites ? true : false}
-                icon={<FavoriteBorder />}
-                checkedIcon={<Favorite />}
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+							<div className='product__info'>
+								<p className='product__desc'>
+									<strong>{x.name} </strong> {truncate(x.description)}
+								</p>
+
+								<div className='rating'>
+									<Rating name='read-only' value={x.rating} readOnly />
+									<span className='reviews'>
+										{x.numReviews} <RemoveRedEyeIcon></RemoveRedEyeIcon>
+									</span>
+								</div>
+								<div className='price'>
+									<p>Basket Price</p>
+									<CurrencyFormat value={x.price} displayType={"text"} thousandSeparator={true} prefix={"$"} />
+								</div>
+							</div>
+						</Link>
+						<div className='heart'>
+							<Checkbox
+								onClick={() => dispatch(addFavorites(x.productUnicId))}
+								sx={{
+									"&.Mui-checked": {
+										color: orange[600],
+									},
+								}}
+								checked={x.chechFavorites ? true : false}
+								icon={<FavoriteBorder />}
+								checkedIcon={<Favorite />}
+							/>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Laptop;
--- src/components/LaptopFilter.jsx
+++ src/components/LaptopFilter.jsx
@@ -9,174 +9,172 @@ import { useDispatch } from "react-redux";
 import { handleFilter } from "../redux/commerceSlice.js";
 
 const LaptopFilter = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const [productBrand, setProductBrand] = useState({
-    brands: [],
-  });
+	const [productBrand, setProductBrand] = useState({
+		brands: [],
+	});
 
-  const handleChange = (e) => {
-    // Destructuring
-    const { value, checked } = e.target;
-    const { brands } = productBrand;
+	const handleChange = (e) => {
+		// Destructuring
+		const { value, checked } = e.target;
+		const { brands } = productBrand;
 
-    // Case 1 : The user checks the box
-    if (checked) {
-      setProductBrand({
-        brands: [...brands, value],
-      });
-    }
+		// Case 1 : The user checks the box
+		if (checked) {
+			setProductBrand({
+				brands: [...brands, value],
+			});
+		}
 
-    // Case 2  : The user unchecks the box
-    else {
-      setProductBrand({
-        brands: brands.filter((e) => e !== value),
-      });
-    }
-  };
+		// Case 2  : The user unchecks the box
+		else {
+			setProductBrand({
+				brands: brands.filter((e) => e !== value),
+			});
+		}
+	};
 
-  dispatch(handleFilter(productBrand));
+	dispatch(handleFilter(productBrand));
 
-  const [drop, setDrop] = useState(null);
+	const [drop, setDrop] = useState(null);
 
-  const handleDrop = () => {
-    setDrop(!drop);
-  };
+	const handleDrop = () => {
+		setDrop(!drop);
+	};
 
-  return (
-    <div className="filter__container">
-      <div className="filter__brand">
-        <div onClick={() => handleDrop()} className="filter__header">
-          <h3>Brand</h3>
-          <KeyboardArrowDownIcon
-            className={`arrow__icon ${drop ? "active" : "null"}`}
-          ></KeyboardArrowDownIcon>
-        </div>
+	return (
+		<div className='filter__container'>
+			<div className='filter__brand'>
+				<div onClick={() => handleDrop()} className='filter__header'>
+					<h3>Brand</h3>
+					<KeyboardArrowDownIcon className={`arrow__icon ${drop ? "active" : "null"}`}></KeyboardArrowDownIcon>
+				</div>
 
-        <div className={`filter__inner__con ${drop ? "active" : "null"}`}>
-          <div className="filter__inner">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={(e) => handleChange(e)}
-                    value="Apple"
-                    size="small"
-                    sx={{
-                      "&.Mui-checked": {
-                        color: orange[600],
-                      },
-                    }}
-                  />
-                }
-                label="Apple"
-              />
-            </FormGroup>
-          </div>
+				<div className={`filter__inner__con ${drop ? "active" : "null"}`}>
+					<div className='filter__inner'>
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Checkbox
+										onChange={(e) => handleChange(e)}
+										value='Apple'
+										size='small'
+										sx={{
+											"&.Mui-checked": {
+												color: orange[600],
+											},
+										}}
+									/>
+								}
+								label='Apple'
+							/>
+						</FormGroup>
+					</div>
 
-          <div className="filter__inner">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={(e) => handleChange(e)}
-                    value="Casper"
-                    sx={{
-                      "&.Mui-checked": {
-                        color: orange[600],
-                      },
-                    }}
-                    size="small"
-                  />
-                }
-                label="Casper"
-              />
-            </FormGroup>
-          </div>
+					<div className='filter__inner'>
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Checkbox
+										onChange={(e) => handleChange(e)}
+										value='Casper'
+										sx={{
+											"&.Mui-checked": {
+												color: orange[600],
+											},
+										}}
+										size='small'
+									/>
+								}
+								label='Casper'
+							/>
+						</FormGroup>
+					</div>
 
-          <div className="filter__inner">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={(e) => handleChange(e)}
-                    size="small"
-                    value="Hp"
-                    sx={{
-                      "&.Mui-checked": {
-                        color: orange[600],
-                      },
-                    }}
-                  />
-                }
-                label="Hp"
-              />
-            </FormGroup>
-          </div>
+					<div className='filter__inner'>
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Checkbox
+										onChange={(e) => handleChange(e)}
+										size='small'
+										value='Hp'
+										sx={{
+											"&.Mui-checked": {
+												color: orange[600],
+											},
+										}}
+									/>
+								}
+								label='Hp'
+							/>
+						</FormGroup>
+					</div>
 
-          <div className="filter__inner">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={(e) => handleChange(e)}
-                    size="small"
-                    value="Asus"
-                    sx={{
-                      "&.Mui-checked": {
-                        color: orange[600],
-                      },
-                    }}
-                  />
-                }
-                label="Asus"
-              />
-            </FormGroup>
-          </div>
+					<div className='filter__inner'>
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Checkbox
+										onChange={(e) => handleChange(e)}
+										size='small'
+										value='Asus'
+										sx={{
+											"&.Mui-checked": {
+												color: orange[600],
+											},
+										}}
+									/>
+								}
+								label='Asus'
+							/>
+						</FormGroup>
+					</div>
 
-          <div className="filter__inner">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={(e) => handleChange(e)}
-                    size="small"
-                    value="Monster"
-                    sx={{
-                      "&.Mui-checked": {
-                        color: orange[600],
-                      },
-                    }}
-                  />
-                }
-                label="Monster"
-              />
-            </FormGroup>
-          </div>
+					<div className='filter__inner'>
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Checkbox
+										onChange={(e) => handleChange(e)}
+										size='small'
+										value='Monster'
+										sx={{
+											"&.Mui-checked": {
+												color: orange[600],
+											},
+										}}
+									/>
+								}
+								label='Monster'
+							/>
+						</FormGroup>
+					</div>
 
-          <div className="filter__inner">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    onChange={(e) => handleChange(e)}
-                    size="small"
-                    value="Msi"
-                    sx={{
-                      "&.Mui-checked": {
-                        color: orange[600],
-                      },
-                    }}
-                  />
-                }
-                label="Msi"
-              />
-            </FormGroup>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className='filter__inner'>
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Checkbox
+										onChange={(e) => handleChange(e)}
+										size='small'
+										value='Msi'
+										sx={{
+											"&.Mui-checked": {
+												color: orange[600],
+											},
+										}}
+									/>
+								}
+								label='Msi'
+							/>
+						</FormGroup>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LaptopFilter;
